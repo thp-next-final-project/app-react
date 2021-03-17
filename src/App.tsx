ca2addf73ec12c9571f13433011a07202e199081
rtdeedqgryet <borismariondorier@gmail.com>
2021-03-17 19:06:36 +0100

    update forms

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,8 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
-import Navbar from './components/Layout/NavBar';
-import { Footer } from './components/Layout/Footer';
+import Navbar from './components/layout/NavBar';
+import { Footer } from './components/layout/footer';
 
 const App = (): JSX.Element => {
   const user:any = useSelector((state) => state);
