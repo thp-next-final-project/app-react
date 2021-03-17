14e180b17d00ac6f4f7e49bfcb04a3666386e766
rtdeedqgryet <borismariondorier@gmail.com>
2021-03-17 03:28:43 +0100

    created 'glass' mixin with calculation

--- src/pages/homepage/index.jsx
+++ src/pages/homepage/index.jsx
@@ -3,7 +3,37 @@ import React from 'react';
 
 export function Homepage () {
     return (
-        <div>hahaha</div>
+        <div>
+            <div className="jumbotron">
+                <div className="header">
+                    coucou
+                </div>
+            </div>
+            <div className="main">
+                <div className="right">
+
+                </div>
+                <div className="left">
+                    <h2 className="title">
+
+                    </h2>
+                    <div className="sub-container">
+                        <div className="card">
+
+                        </div>
+                        <div className="card">
+
+                        </div>
+                        <div className="card">
+
+                        </div>
+                        <div className="card">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 
 };
